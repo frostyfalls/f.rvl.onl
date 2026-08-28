fa2b9b97cfc648d76947fd82ac3a1c4763454886
frosty <f@rvl.onl>
2026-08-28 02:46:19 -0400

    add opengraph tags, clean up template function

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ outdir=public
 contentdir=content
 
 # https://romanzolotarev.com/ssg/ssg.sh
-mustache() {
+tmpl() {
 	d=0
 	s=-1
 	while IFS= read -r line; do
@@ -12,39 +12,33 @@ mustache() {
 		rest=$line
 		while :; do
 			case $rest in
-			*'{{'*)
+			*{{*)
 				before=${rest%%\{\{*}
 				rest=${rest#*\{\{}
 				tag=${rest%%\}\}*}
 				rest=${rest#*\}\}}
-				if test $s -lt 0; then o="${o}${before}"; fi
+				[ $s -lt 0 ] && o="$o$before"
 				case $tag in
-				'#'*)
-					d=$((d + 1))
-					if test $s -lt 0 -a -z "$(printenv "${tag#\#}")"; then s=$d; fi
-					;;
-				'/'*) if test $s -eq $d; then s=-1; fi && d=$((d - 1)) ;;
-				'^'*)
-					d=$((d + 1))
-					if test $s -lt 0 -a -n "$(printenv "${tag#^}")"; then s=$d; fi
-					;;
-				*) if test $s -lt 0; then o="${o}$(printenv "$tag")"; fi ;;
+				\#*) d=$((d + 1)); [ $s -lt 0 ] && [ -z "$(printenv "${tag#\#}")" ] && s=$d ;;
+				^*) d=$((d + 1)); [ $s -lt 0 ] && [ -n "$(printenv "${tag#^}")" ] && s=$d ;;
+				/*) [ $s -eq $d ] && s=-1; d=$((d - 1)) ;;
+				*) [ $s -lt 0 ] && o="${o}$(printenv "$tag")" ;;
 				esac
 				;;
-			*) if test $s -lt 0; then o="${o}${rest}"; fi && break ;;
+			*) [ $s -lt 0 ] && o="${o}${rest}"; break ;;
 			esac
 		done
-		if test $s -lt 0 -o -n "$o"; then printf '%s\n' "$o"; fi
-		# printf '%s\n' "$o"
+		[ $s -lt 0 ] || [ -n "$o" ] && printf '%s\n' "$o"
 	done
 }
-
 
 if [ -d "$outdir" ]; then
 	rm -rf "$outdir"
 fi
 mkdir -p "$outdir"
 
+domain=https://f.rvl.onl
+export url=$domain
 {
 	cat base.pre.html
 	printf '<h2>Bits and tips</h2>\n'
@@ -53,7 +47,8 @@ mkdir -p "$outdir"
 		slug=${f#content/bits.}
 		slug=${slug%.md}
 		export title=$(lowdown -X title "$f") \
-			lastmod=$(lowdown -X lastmod "$f")
+			lastmod=$(lowdown -X lastmod "$f") \
+			url="$domain/$slug.html"
 		printf '<li>%s: <a href="%s">%s</a></li>\n' \
 			"$(date -d "$lastmod" "+%b %d %Y")" "/$slug.html" "$title"
 		{
@@ -63,10 +58,10 @@ mkdir -p "$outdir"
 			printf '<p>Last modified %s.</p>\n' \
 				"$(date -d "$lastmod" "+%b %d %Y")"
 			cat base.post.html
-		} | mustache >"$outdir/$slug.html"
+		} | tmpl >"$outdir/$slug.html"
 	done
 	unset slug title lastmod
 	printf '</ul>\n'
 	cat base.post.html
-} | mustache >"$outdir/index.html"
+} | tmpl >"$outdir/index.html"
 cp style.css "$outdir"
